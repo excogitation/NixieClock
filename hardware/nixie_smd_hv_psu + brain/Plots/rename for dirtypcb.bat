623af88ede1aac2dfc2a8d340aee7f308b91e203
Christian Stöveken <Christian.Stoeveken@gmail.com>
2017-05-20 01:58:06 +0200

    added DesignSpark PCB project files
    + export options + conversion script for OSHpark as well as dirtypcb
    (seedstudio)
    as we now ordered the first PCB's

---
+++ hardware/nixie_smd_hv_psu + brain/Plots/rename for dirtypcb.bat
@@ -0,0 +1,18 @@
+del boardname.GKO 
+del boardname.GBL
+del boardname.GBO
+del boardname.GBS
+del boardname.DRL
+del boardname.GTL
+del boardname.GTO
+del boardname.GTS
+
+ren "Board outline.gbr" boardname.GKO 
+ren "Bottom.gbr" boardname.GBL
+ren "Bot Silk.gbr" boardname.GBO
+ren "Bot Mask.gbr" boardname.GBS
+ren "Drill Data - [Through Hole].drl" boardname.DRL
+ren "Top.gbr" boardname.GTL
+ren "Top Silk.gbr" boardname.GTO
+ren "Top Mask.gbr" boardname.GTS
+
